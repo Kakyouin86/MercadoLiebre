9c6d00442f6bd4a25d63fac2a6850b1a9264871c
Leonardo Jorge Monge <leo.monge1986@gmail.com>
2023-05-28 14:20:04 -0300

    Commit 1

---
+++ app.js
@@ -0,0 +1,11 @@
+//npm init
+//npm install express
+const express = require('express');
+const path = require('path');
+const app = express();
+const publicPath = path.resolve(__dirname, './public');
+//app.use(express.static(publicPath));
+app.use(express.static(path.join(__dirname, '/public')))
+app.listen(3000, () => { console.log("Servidor corriendo en puerto 3000") });
+app.get('/', (req, res) => { res.sendFile(path.resolve(__dirname, './views/home.html')) });
+//app.get('/', (req, res) => { res.sendFile((__dirname + '/views/home.html')) });
