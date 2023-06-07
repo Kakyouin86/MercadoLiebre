154ac946050e2729e6b5f301e8d86d7cde8a91bd
Leonardo Jorge Monge <leo.monge1986@gmail.com>
2023-06-07 20:14:03 -0300

    Updates, pero falta

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ const publicPath = path.resolve(__dirname, './public');
 //app.use(express.static(publicPath));
 app.use(express.static(path.join(__dirname, '/public')))
 app.listen(3000, () => { console.log("Servidor corriendo en puerto 3000") });
-app.get('/', (req, res) => { res.sendFile(path.resolve(__dirname, './views/home.html')) });
+app.get('/', (req, res) => { res.sendFile(path.resolve(__dirname, './views/login.html')) });
 //app.get('/', (req, res) => { res.sendFile((__dirname + '/views/home.html')) });
